03ac6df551d62957dce6d99578060597a293787e
Lee sangmi <sangmidi1232@naver.com>
2023-07-16 20:18:56 +0900

    조건문 문제 완료

---
+++ 2588.py
@@ -0,0 +1,16 @@
+a = int(input())
+b = input()
+c = int(b[2])
+d = int(b[1])
+e = int(b[0])
+
+var1 = a * c
+var2 = a * d
+var3 = a * e
+
+var =  var1 + var2 * 10 + var3 *100 
+
+print(var1)
+print(var2)
+print(var3)
+print(var)
